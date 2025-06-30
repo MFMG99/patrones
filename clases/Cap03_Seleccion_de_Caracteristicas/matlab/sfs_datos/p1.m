b3b8ce700549fcebbbfa92d64b2f12ce9df788db
Maite Madalosso <48294273+MFMG99@users.noreply.github.com>
2025-06-30 13:49:01 -0400

    Delete clases/Cap03_Seleccion_de_Caracteristicas/matlab directory

--- clases/Cap03_Seleccion_de_Caracteristicas/matlab/sfs_datos/p1.m
+++
@@ -1,7 +0,0 @@
-% Ver enunciado en enunciado.pdf
-% D. Mery, Abril 2019
-% Universidad Cat?lica de Chile
-load DATOS1
-op.k = 1;
-Ypred = Bcl_knn(Xtrain,Ytrain,Xtest,op);
-Bev_performance(Ypred,Ytest)
